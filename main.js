fd1ed6dc89b32e9e39cd93f21ca40c8ba8d01f9e
3DWARDRG <Edwardr185@gmail.com>
2023-06-04 23:00:15 -0500

    realize el ejercicio 5

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+
+// Ejercicio 1
+
+/* Este código solicita al usuario que ingrese un número. Si el usuario ingresa un valor no numérico, seguirá solicitando al usuario que 
+ingrese un número hasta que se ingrese un valor numérico. Una vez que se ingresa un valor numérico, el código calculará los múltiplos 
+de 5 desde 1 hasta el número ingresado y los almacenará en una matriz llamada mulpofthefive. 
+Finalmente, mostrará una alerta con la matriz de múltiplos de 5.*/
+
 /*
+
+
 let inputnumber=prompt("Introduce un numero");
 let i=1;
 let mulpofthefive=[];
@@ -17,7 +27,13 @@ alert(mulpofthefive)
 
 */
 
+
 /*
+
+
+/*
+
+// Ejercicio 2
 
 let firstinputnumber=validationOfRange(prompt("Introduce un numero en el rango de 1 al 50."));
 
@@ -133,4 +149,52 @@ alert("Tu escribiste esto " + arrayStrings + ".")
 
       */
 
+
+    // Ejercicio 5
+
+    let DayOfTheWeek=["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];
+
+    let inputDayOfTheWeek= validationOfInformation(prompt("Dame un dia de la semana.").toLowerCase());
+
+    while(inputDayOfTheWeek !== "domingo"){
+
+       switch(inputDayOfTheWeek){
+        case"lunes":
+        alert("Si no te gustan los lunes, no crees en las segundas oportunidades para ti.");
+        break;
+        case "martes":
+        alert(" “El éxito no es la clave de la felicidad. La felicidad es la clave del éxito. Si amas lo que estás haciendo, tendrás éxito.” - Albert Schweitzer. ");
+        break;
+       }      
+
+       inputDayOfTheWeek= prompt("Dame un dia de la semana.(ej: lunes, jueves, domingo, etc).").toLowerCase();
+
+    }
+
+
+    if(inputDayOfTheWeek==="domingo"){
+        alert("Ve a descansar.")
+    }
+
+    else{
+        alert("Introduce valores validos, vuelve a intentarlo");
+    }
+
+
+
+
+
+
+    function validationOfInformation(a){
+
+        while(!DayOfTheWeek.includes(a)){
+
+            a= prompt("Dame un dia de la semana.").toLowerCase();
+
+        }
+
+        return a; 
+        }
+
       
+    
